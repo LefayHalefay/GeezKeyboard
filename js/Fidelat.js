0379e1404a8feda793bb758c38e8f209b89d0c41
Minasie Yosief <Minasie.Ghebrehiwet@gmail.com>
2018-05-05 22:22:29 -0500

    Major Commit: Finished Geez Alphabets to English Mapping, added Alphabets Dropdown, and Finished basic UI.

--- js/Fidelat.js
+++ js/Fidelat.js
@@ -14,25 +14,32 @@ var giveMeMySpace = "&nbsp;";
 var HE = "\u1200"; var HU = "\u1201"; var HI = "\u1202"; var HA = "\u1203"; var HIE = "\u1204"; var HH = "\u1205"; var HO = "\u1206"; 
 
 /*(2) THE L FAMILY*/
-var LE = "\u1208"; var LU = "\u1209"; var LI = "\u120A"; var LA = "\u120B"; var LIE = "\u120C"; var LL = "\u120D"; var LO = "\u120E"; var LUA = "\u120F";
+var LE = "\u1208"; var LU = "\u1209"; var LI = "\u120A"; var LA = "\u120B"; var LIE = "\u120C"; var LL = "\u120D"; var LO = "\u120E"; 
+var LUA = "\u120F";
 
 /* (3)THE HH FAMILY*/
-var HHE = "\u1210"; var HHU = "\u1211"; var HHI = "\u1212"; var HHA = "\u1213"; var HHIE = "\u1214"; var HHH = "\u1215"; var HHO = "\u1216"; var HHUA = "\u1217";
+var HHE = "\u1210"; var HHU = "\u1211"; var HHI = "\u1212"; var HHA = "\u1213"; var HHIE = "\u1214"; var HHH = "\u1215"; var HHO = "\u1216"; 
+var HHUA = "\u1217";
 
 /*(4) THE M FAMILY*/
-var ME = "\u1218"; var MU = "\u1219"; var MI = "\u121A"; var MA = "\u121B"; var MIE = "\u121C"; var MM = "\u121D"; var MO = "\u121E"; var MUA = "\u121F";
+var ME = "\u1218"; var MU = "\u1219"; var MI = "\u121A"; var MA = "\u121B"; var MIE = "\u121C"; var MM = "\u121D"; var MO = "\u121E"; 
+var MUA = "\u121F";
 
 /*(5) THE SS FAMILY(The GEEZ type)*/
-var SSE = "\u1220"; var SSU = "\u1221"; var SSI = "\u1222"; var SSA = "\u1223"; var SSIE = "\u1224"; var SS = "\u1225"; var SSO = "\u1226"; var SSUA = "\u1227";
+var SSE = "\u1220"; var SSU = "\u1221"; var SSI = "\u1222"; var SSA = "\u1223"; var SSIE = "\u1224"; var SSS = "\u1225"; var SSO = "\u1226"; 
+var SSUA = "\u1227";
 
 /*(6) THE R FAMILY*/
-var RE = "\u1228"; var RU = "\u1229"; var RI = "\u122A"; var RA = "\u122B"; var RIE = "\u122C"; var RR = "\u122D"; var RO = "\u122E"; var RUA = "\u122F";
+var RE = "\u1228"; var RU = "\u1229"; var RI = "\u122A"; var RA = "\u122B"; var RIE = "\u122C"; var RR = "\u122D"; var RO = "\u122E"; 
+var RUA = "\u122F";
 
 /*(7) THE S FAMILY*/
-var SE = "\u1230"; var SU = "\u1231"; var SI = "\u1232"; var SA = "\u1233"; var SIE = "\u1234"; var SS = "\u1235"; var SO = "\u1236"; var SUA = "\u1237";
+var SE = "\u1230"; var SU = "\u1231"; var SI = "\u1232"; var SA = "\u1233"; var SIE = "\u1234"; var SS = "\u1235"; var SO = "\u1236"; 
+var SUA = "\u1237";
 
 /*(8) THE SH FAMILY*/
-var SHE = "\u1238"; var SHU = "\u1239"; var SHI = "\u123A"; var SHA = "\u123B"; var SHIE = "\u123C"; var SH = "\u123D"; var SHO = "\u123E"; var SHUA = "\u123F";
+var SHE = "\u1238"; var SHU = "\u1239"; var SHI = "\u123A"; var SHA = "\u123B"; var SHIE = "\u123C"; var SH = "\u123D"; var SHO = "\u123E"; 
+var SHUA = "\u123F";
 
 /*(9) THE Q FAMILY*/
 var QE = "\u1240"; var QU = "\u1241"; var QI = "\u1242"; var QA = "\u1243"; var QIE = "\u1244"; var QQ = "\u1245"; var QO = "\u1246"; 
@@ -40,28 +47,35 @@ var QOH = "\u1248"; var QWI = "\u124A"; var QUA = "\u124B"; var QWIE = "\u124C";
 
 /*(10) THE QH FAMILY(This letter is ONLY in Tigriyna)*/
 var QHE = "\u1250"; var QHU = "\u1251"; var QHI = "\u1252"; var QHA = "\u1253"; var QHIE = "\u1254"; var QH = "\u1255"; var QHO = "\u1256"; 
-var QHOH = "\u1258"; var QHWI = "\u125A"; var QHUA = "\u125B"; var QHWIE = "\u124C"; var QHWII = "\u125D";
+var QHOH = "\u1258"; var QHWI = "\u125A"; var QHUA = "\u125B"; var QHWIE = "\u125C"; var QHWII = "\u125D";
 
 /*(11) THE B FAMILY*/
-var BE = "\u1260"; var BU = "\u1261"; var BI = "\u1262"; var BA = "\u1263"; var BIE = "\u1264"; var BB = "\u1265"; var BO = "\u1262"; var BUA = "\u1267";
+var BE = "\u1260"; var BU = "\u1261"; var BI = "\u1262"; var BA = "\u1263"; var BIE = "\u1264"; var BB = "\u1265"; var BO = "\u1266"; 
+var BUA = "\u1267";
 
 /*(12) THE V FAMILY*/
-var VE = "\u1268"; var VU = "\u1269"; var VI = "\u126A"; var VA = "\u126B"; var VIE = "\u126C"; var VV = "\u126D"; var VO = "\u126E"; var VUA = "\u126F";
+var VE = "\u1268"; var VU = "\u1269"; var VI = "\u126A"; var VA = "\u126B"; var VIE = "\u126C"; var VV = "\u126D"; var VO = "\u126E"; 
+var VUA = "\u126F";
 
 /*(13) THE T FAMILY*/
-var TE = "\u1270"; var TU = "\u1271"; var TI = "\u1272"; var TA = "\u1273"; var TIE = "\u1274"; var TT = "\u1275"; var TO = "\u1276"; var TUA = "\u1277";
+var TE = "\u1270"; var TU = "\u1271"; var TI = "\u1272"; var TA = "\u1273"; var TIE = "\u1274"; var TT = "\u1275"; var TO = "\u1276"; 
+var TUA = "\u1277";
 
 /*(14) THE CH FAMILY (Latin Inspired CH)*/
-var CHE = "\u1278"; var CHU = "\u1279"; var CHI = "\u127A"; var CHA = "\u127B"; var CHIE = "\u127C"; var CH = "\u127D"; var CHO = "\u127E"; var CHUA = "\u127F";
+var CE = "\u1278"; var CU = "\u1279"; var CI = "\u127A"; var CA = "\u127B"; var CIE = "\u127C"; var CC = "\u127D"; var CO = "\u127E"; 
+var CHUA = "\u127F";
 
 /*(15) THE N FAMILY*/
-var NE = "\u1290"; var NU = "\u1291"; var NI = "\u1292"; var NA = "\u1293"; var NIE = "\u1294"; var NN = "\u1295"; var NO = "\u1296"; var NUA = "\u1297";
+var NE = "\u1290"; var NU = "\u1291"; var NI = "\u1292"; var NA = "\u1293"; var NIE = "\u1294"; var NN = "\u1295"; var NO = "\u1296"; 
+var NUA = "\u1297";
 
 /*(16) THE NG FAMILY*/
-var NGE = "\u1298"; var NGU = "\u1299"; var NGI = "\u129A"; var NGA = "\u129B"; var NGIE = "\u129C"; var NG = "\u129D"; var NGO = "\u129E"; var NGUA = "\u129F";
+var NGE = "\u1298"; var NGU = "\u1299"; var NGI = "\u129A"; var NGA = "\u129B"; var NGIE = "\u129C"; var NG = "\u129D"; var NGO = "\u129E"; 
+var NGUA = "\u129F";
 
 /*(17) THE E FAMILY*/
-var EE = "\u12A0"; var EU = "\u12A1"; var EI = "\u12A2"; var EA = "\u12A3"; var EIE = "\u12A4"; var EEH = "\u12A5"; var EO = "\u12A6"; var EUA = "\u12A7";
+var EE = "\u12A0"; var EU = "\u12A1"; var EI = "\u12A2"; var EA = "\u12A3"; var EIE = "\u12A4"; var EEH = "\u12A5"; var EO = "\u12A6"; 
+var EUA = "\u12A7";
 
 /*(18) THE K FAMILY*/
 var KE = "\u12A8"; var KU = "\u12A9"; var KI = "\u12AA"; var KA = "\u12AB"; var KIE = "\u12AC"; var KK = "\u12AD"; var KO = "\u12AE"; 
@@ -72,50 +86,63 @@ var KHE = "\u12B8"; var KHU = "\u12B9"; var KHI = "\u12BA"; var KHA = "\u12BB"; 
 var KHWO = "\u12C0"; var KHWI = "\u12C2"; var KHUA = "\u12C3"; var KHWE = "\u12C4"; var KHWUE = "\u12C5";
 
 /*(20) THE W FAMILY*/
-var WE = "\u12C8"; var WU = "\u12C9"; var WI = "\u12CA"; var WA = "\u12CB"; var WIE = "\u12CC"; var W = "\u12CD"; var WO = "\u12CE"; var WOA = "\u12CF";
+var WE = "\u12C8"; var WU = "\u12C9"; var WI = "\u12CA"; var WA = "\u12CB"; var WIE = "\u12CC"; var WW = "\u12CD"; var WO = "\u12CE"; 
+var WUA = "\u12CF";
 
 /*(21) THE EH FAMILY(THIS LETTER IS ONLY IN TIGRIYNA)*/
 var EHE = "\u12D0"; var EHU = "\u12D1"; var EHI = "\u12D2"; var EHA = "\u12D3"; var EHIE = "\u12D4"; var EH = "\u12D5"; var EHO = "\u12D6";
 
 /*(22) THE Z FAMILY*/
-var ZE = "\u12D8"; var ZU = "\u12D9"; var ZI = "\u12DA"; var ZA = "\u12DB"; var ZIE = "\u12DC"; var ZZ = "\u12DD"; var ZO = "\u12DE"; var ZUA = "\u12DF";
+var ZE = "\u12D8"; var ZU = "\u12D9"; var ZI = "\u12DA"; var ZA = "\u12DB"; var ZIE = "\u12DC"; var ZZ = "\u12DD"; var ZO = "\u12DE"; 
+var ZUA = "\u12DF";
 
 /*(23) THE ZH FAMILY*/
-var ZHE = "\u12E0"; var ZHU = "\u12E1"; var ZHI = "\u12E2"; var ZHA = "\u12E3"; var ZHIE = "\u12E4"; var ZH = "\u12E5"; var ZHO = "\u12E6"; var ZHUA = "\u12E7";
+var ZHE = "\u12E0"; var ZHU = "\u12E1"; var ZHI = "\u12E2"; var ZHA = "\u12E3"; var ZHIE = "\u12E4"; var ZH = "\u12E5"; var ZHO = "\u12E6"; 
+var ZHUA = "\u12E7";
 
 /*(24) THE Y FAMILY*/
-var YE = "\u12E8"; var YU = "\u12E9"; var YI = "\u12EA"; var YA = "\u12EB"; var YIE = "\u12EC"; var YY = "\u12ED"; var YO = "\u12EE"; var YUA = "\u12EF";
+var YE = "\u12E8"; var YU = "\u12E9"; var YI = "\u12EA"; var YA = "\u12EB"; var YIE = "\u12EC"; var YY = "\u12ED"; var YO = "\u12EE"; 
+var YUA = "\u12EF";
 
 /*(25) THE D FAMILY*/
-var DE = "\u12F0"; var DU = "\u12F1"; var DI = "\u12F2"; var DA = "\u12F3"; var DIE = "\u12F4"; var DD = "\u12F5"; var DO = "\u12F6"; var DUA = "\u12F7";
+var DE = "\u12F0"; var DU = "\u12F1"; var DI = "\u12F2"; var DA = "\u12F3"; var DIE = "\u12F4"; var DD = "\u12F5"; var DO = "\u12F6"; 
+var DUA = "\u12F7";
 
 /*(26) THE J FAMILY*/
-var JE = "\u1300"; var JU = "\u1301"; var JI = "\u1302"; var JA = "\u1303"; var JIE = "\u1304"; var JJ = "\u1305"; var JO = "\u1306"; var JUA = "\u1307";
+var JE = "\u1300"; var JU = "\u1301"; var JI = "\u1302"; var JA = "\u1303"; var JIE = "\u1304"; var JJ = "\u1305"; var JO = "\u1306"; 
+var JUA = "\u1307";
 
 /*(27) THE G FAMILY*/
 var GE = "\u1308"; var GU = "\u1309"; var GI = "\u130A"; var GA = "\u130B"; var GIE = "\u130C"; var GG = "\u130D"; var GO = "\u130E"; 
 var GWO = "\u1310"; var GWI = "\u1312"; var GUA = "\u1313"; var GWE = "\u1314"; var GWUE = "\u1315";
 
 /*(28) THE TH FAMILY*/
-var THE = "\u1320"; var THU = "\u1321"; var THI = "\u1322"; var THA = "\u1323"; var THIE = "\u1324"; var TH = "\u1325"; var THO = "\u1326"; var THUA = "\u1327";
+var THE = "\u1320"; var THU = "\u1321"; var THI = "\u1322"; var THA = "\u1323"; var THIE = "\u1324"; var TH = "\u1325"; var THO = "\u1326"; 
+var THUA = "\u1327";
 
 /*(29) THE CHH FAMILY*/
-var CHHE = "\u1328"; var CHHU = "\u1329"; var CHHI = "\u132A"; var CHHA = "\u132B"; var CHHIE = "\u132C"; var CHH = "\u132D"; var CHHO = "\u132E"; var CHHUA = "\u132F";
+var CHE = "\u1328"; var CHU = "\u1329"; var CHI = "\u132A"; var CHA = "\u132B"; var CHIE = "\u132C"; var CH = "\u132D"; var CHO = "\u132E"; 
+var CHUA = "\u132F";
 
 /*(30) THE PH FAMILY*/
-var PH = "\u1330"; var PHU = "\u1331"; var PHI = "\u1332"; var PHA = "\u1333"; var PHIE = "\u1334"; var PH = "\u1335"; var PHO = "\u1336"; var PHUA = "\u1337";
+var PHE = "\u1330"; var PHU = "\u1331"; var PHI = "\u1332"; var PHA = "\u1333"; var PHIE = "\u1334"; var PH = "\u1335"; var PHO = "\u1336"; 
+var PHUA = "\u1337";
 
 /*(31) THE TS FAMILY*/
-var TSE = "\u1338"; var TSU = "\u1339"; var TSI = "\u133A"; var TSA = "\u133B"; var TSIE = "\u133C"; var TS = "\u133D"; var TSO = "\u133E"; var TSUA = "\u133F";
+var TSE = "\u1338"; var TSU = "\u1339"; var TSI = "\u133A"; var TSA = "\u133B"; var TSIE = "\u133C"; var TS = "\u133D"; var TSO = "\u133E"; 
+var TSUA = "\u133F";
 
 /*(32) THE TZ(THE OTHER TSE FAIMILY. AMHARIC AND GEEZ USE IT) FAMILY*/
-var TZE = "\u1340"; var TZU = "\u1341"; var TZI = "\u1342"; var TZA = "\u1343"; var TZIE = "\u1344"; var TZ = "\u1345"; var TZO = "\u1346"; var TZUA = "\u1347";
+var TZE = "\u1340"; var TZU = "\u1341"; var TZI = "\u1342"; var TZA = "\u1343"; var TZIE = "\u1344"; var TZ = "\u1345"; var TZO = "\u1346"; 
+var TZUA = "\u1347";
 
 /*(33) THE FE FAMILY*/
-var FE = "\u1348"; var FU = "\u1349"; var FI = "\u134A"; var FA = "\u134B"; var FIE = "\u134C"; var FF = "\u134D"; var FO = "\u134E"; var FUA = "\u134F";
+var FE = "\u1348"; var FU = "\u1349"; var FI = "\u134A"; var FA = "\u134B"; var FIE = "\u134C"; var FF = "\u134D"; var FO = "\u134E";
+ var FUA = "\u134F";
 
 /*(34) THE PE FAMILY*/
-var PE = "\u1350"; var PU = "\u1351"; var PI = "\u1352"; var PA = "\u1353"; var PIE = "\u1354"; var PP = "\u1355"; var PO = "\u1356"; var PUA = "\u1357";
+var PE = "\u1350"; var PU = "\u1351"; var PI = "\u1352"; var PA = "\u1353"; var PIE = "\u1354"; var PP = "\u1355"; var PO = "\u1356"; 
+var PUA = "\u1357";
 
 
 
